f8d7556f245ad086388ab109557464a8970a6c46
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2015-06-30 14:12:48 +0200

    Fetch new data btn: use loading... feature

--- opus20/webapp/static/js/script.js
+++ opus20/webapp/static/js/script.js
@@ -2,18 +2,18 @@
 
 $(function(){
   $('#fetchData').on('click', function(){
-    //console.log('trying to fetch data...');
+    var $btn = $(this).button('loading')
     $.ajax({
       url: "/download/" + device_id,
       type: 'get',
       success: function (response) {
         //console.log('response received');
         location.reload()
-        // ajax success callback
+        $btn.button('reset')
       }, error: function (response) {
         console.log('ajax request to fetch data failed');
         alert('ajax request to fetch data failed');
-        // ajax error callback
+        $btn.button('reset')
       },
     });
   });
