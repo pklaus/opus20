5828c05b425365940fee04b3983291504e2487cf
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2015-07-01 09:27:47 +0200

    Migrating from the stateful .button() API to .disabled property
    .button('loading') and .button('reset') are marked as deprecated:
    http://getbootstrap.com/javascript/#buttons-stateful

--- opus20/webapp/static/js/script.js
+++ opus20/webapp/static/js/script.js
@@ -2,18 +2,23 @@
 
 $(function(){
   $('#fetchData').on('click', function(){
-    var $btn = $(this).button('loading')
+    var $btn = $(this);
+    $btn.prop('disabled', true);
+    var $text = $btn[0].textContent;
+    $btn.prop('textContent', "Loading...");
     $.ajax({
       url: "/download/" + device_id,
       type: 'get',
       success: function (response) {
         //console.log('response received');
-        $btn.button('reset')
+        $btn.prop('disabled', false);
+        $btn.prop('textContent', $text);
         location.reload()
       }, error: function (response) {
         console.log('ajax request to fetch data failed');
         alert('ajax request to fetch data failed');
-        $btn.button('reset')
+        $btn.prop('disabled', false);
+        $btn.prop('textContent', $text);
       },
     });
   });
